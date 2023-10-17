27b39c7e72504a8ba347281b8c342072884966cf
ranggabn12 <rbaghas32@gmail.com>
2023-10-17 21:14:23 +0700

    tset

--- controllers/indexController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var response = require("../config/res");
-var logger = require("../config/log");
-
-exports.index = function (req, res) {
-  logger.info("Aplikasi berjalan");
-  response.ok([], "Aplikasi berjalan", res);
-};
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var auth = require("../middleware/auth");
-var router = express.Router();
-
-router.post("/api/register", auth.reqistrasi);
-router.post("/api/login", auth.login);
-
-module.exports = router;
